257419359f58c40f2e0ef535f7915534aab488bf
unknown <manish9864040418@gmail.com>
2024-09-04 20:39:41 +0530

    adding logger

--- SpringBootApplication/src/main/java/com/manish/SpringBootApplication/configuration/AppConfig.java
+++ SpringBootApplication/src/main/java/com/manish/SpringBootApplication/configuration/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-	
+	// Creating ModelMapper Bean Configuration
 	@Bean
 	public ModelMapper getModuleMapper() {
 		return new ModelMapper();
